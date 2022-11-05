b64f28d0f0d2c739bc33be459eb708c62f3059dc
Wiyabz <josiahvel6@gmail.com>
2022-11-05 09:43:44 -0600

    merge

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,21 +1,53 @@
 extends KinematicBody2D
 
-export (int) var speed = 80;
-var velocity = Vector2();
+export (int) var speed = 100
+var input = Vector2.ZERO
+var state = IDLE
 
-func get_input():
-	velocity = Vector2();
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1;
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1;
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1;
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1;
+enum {
+	IDLE,
+	MOVE_H,
+	MOVE_V,
+	ATTACK,
+}
+
+onready var animations = $AnimatedSprite
 	
-	velocity = velocity.normalized() * speed;
-
 func _physics_process(delta):
-	get_input();
-	velocity = move_and_slide(velocity);
+	input.x = Input.get_axis("ui_left", "ui_right")
+	input.y = Input.get_axis("ui_up", "ui_down")
+	if input.x != 0:
+		state = MOVE_H
+	elif input.y != 0:
+		state = MOVE_V
+	else: 
+		state = IDLE
+	match state:
+		IDLE:
+			#print('idle')
+			animations.play("Idle")
+		MOVE_H:
+			move_state_H()
+		MOVE_V:
+			move_state_V()
+			
+func move_state_H():
+	var velocity = Vector2.ZERO
+	if input.x == 1:
+		animations.flip_h = true
+	else: 
+		animations.flip_h = false
+	animations.play("Run")
+	velocity = input.normalized() * speed
+	velocity = move_and_slide(velocity)
+	
+func move_state_V():
+	var velocity = Vector2.ZERO
+	if input.y == 1:
+		animations.play("Down")
+	else:
+		animations.play("Up")
+	velocity = input.normalized() * speed
+	velocity = move_and_slide(velocity)
+	
+	
